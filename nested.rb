1f539b2f1d38971b8f65155a0fff3f57c25dc2fd
yash patel <yash82099@yahoo.com>
2019-10-13 00:36:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,9 @@ def hopper
           :languages => ["C"]
         }
      }
+     return programmer_hash[:grace_hopper]
 
-
+  
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
+     
+     
+     
 end
 
 def dennis_ritchies_language
@@ -55,6 +60,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,6 +85,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+     programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP"
+     programmer_hash[:yukihiro_matsumoto][:languages][1] = "C"
 
     
 end
